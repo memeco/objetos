7c63fbb0ad000a61da7d787a988469d5d585a570
Lucas Quadros <olucasquadros@gmail.com>
2024-03-26 20:05:28 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const carro = {
+    marca: 'Ford',
+    preco: 50000,
+    ano: 2020,
+    cor: "branco",
+    motor: 1.0,
+    portas: 4,
+    airbag: true,
+};
+
+let car = document.getElementById('carro');
+car.innerHTML = `<p>Marca: ${carro.marca}</p>
+                 <p>Preço: ${carro.preco}</p>
+                 <p>Ano: ${carro.ano}</p>
+                 <p>Cor: ${carro.cor}</p>
+                 <p>Motor: ${carro.motor}</p>
+                 <p>Portas: ${carro.portas}</p>
+                 <p>Airbag: ${carro.airbag}</p>`;
